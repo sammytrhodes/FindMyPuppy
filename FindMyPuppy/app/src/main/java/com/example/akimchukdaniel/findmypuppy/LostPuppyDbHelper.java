d8d26c39cfe680ced3319b1c282d3631b09bbf5d
Danny Akimchuk <dakimchuk@ithaca.edu>
2016-12-01 05:46:08 -0500

    set up android project, got a sqlite database working, set up a generic list activity which pulls from the database and displays it with an adapter

---
+++ FindMyPuppy/app/src/main/java/com/example/akimchukdaniel/findmypuppy/LostPuppyDbHelper.java
@@ -0,0 +1,48 @@
+package com.example.akimchukdaniel.findmypuppy;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import com.example.akimchukdaniel.findmypuppy.LostPuppy.LostPuppyEntry;
+
+/**
+ * Created by akimchukdaniel on 12/1/16.
+ */
+
+public class LostPuppyDbHelper extends SQLiteOpenHelper {
+
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + LostPuppyEntry.TABLE_NAME + " (" +
+                    LostPuppyEntry._ID + " INTEGER PRIMARY KEY," +
+                    LostPuppyEntry.COLUMN_NAME_NAME + " TEXT," +
+                    LostPuppyEntry.COLUMN_NAME_SEX + " TEXT," +
+                    LostPuppyEntry.COLUMN_NAME_BREED + " TEXT," +
+                    LostPuppyEntry.COLUMN_NAME_FUR_COLOR + " TEXT," +
+                    LostPuppyEntry.COLUMN_NAME_EYE + " TEXT," +
+                    LostPuppyEntry.COLUMN_NAME_LAST_LOCATION + " BLOB," +
+                    LostPuppyEntry.COLUMN_NAME_LAST_TIME + " BLOB )";
+
+    private static final String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS " + LostPuppyEntry.TABLE_NAME;
+
+
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "LostPuppies.db";
+
+
+    public LostPuppyDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_ENTRIES);
+    }
+
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(SQL_DELETE_ENTRIES);
+        onCreate(db);
+    }
+
+    public void onDownGrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        onUpgrade(db, oldVersion, newVersion);
+    }
+}
